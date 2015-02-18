bb34bb6244259560cb7c307b1b5a5e6572fdca95
klambycom <christian@klamby.com>
2015-02-18 23:34:02 +0100

    Add src/

---
+++ src/document.js
@@ -0,0 +1,24 @@
+// TODO Document is document or shadow
+
+var Document = function () {
+};
+
+Document.prototype.add = function () {
+  // TODO Add to JSON to document
+};
+
+Document.prototype.remove = function () {
+  // TODO Remove JSON from document
+};
+
+Document.prototype.update = function () {
+  // TODO Update all JSON
+};
+
+Document.prototype.patch = function () {
+  // TODO Needed? Probably!
+};
+
+Document.prototype.diff = function (shadow) {
+  // TODO Create diff between this and another document, JSON or text.
+};
