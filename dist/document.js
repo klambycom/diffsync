634a86ff32a4b13bde92348b67b1585d201a3a3c
klambycom <christian@klamby.com>
2015-02-18 23:57:15 +0100

    Add dist/

---
+++ dist/document.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// TODO Document is document or shadow
+
+var Document = function () {};
+
+Document.prototype.add = function () {};
+
+Document.prototype.remove = function () {};
+
+Document.prototype.update = function () {};
+
+Document.prototype.patch = function () {};
+
+Document.prototype.diff = function (shadow) {};
+// TODO Add to JSON to document
+// TODO Remove JSON from document
+// TODO Update all JSON
+// TODO Needed? Probably!
+// TODO Create diff between this and another document, JSON or text.
