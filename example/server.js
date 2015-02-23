b78a81955e2e9c2c450b7a3a3f1aa4feaf75db96
klambycom <christian@klamby.com>
2015-02-24 00:38:54 +0100

    Better server

--- example/server.js
+++ example/server.js
@@ -3,9 +3,17 @@ let io = require('socket.io')(app);
 let fs = require('fs');
 
 app.listen(8000);
+console.log('Server started, on port 8000.');
 
 function handler(req, res) {
-  fs.readFile(__dirname + '/client.html', function (err, data) {
+  console.log(new Date(), req.url);
+
+  let file = '/client.html';
+  if (req.url !== '/') {
+    file = req.url;
+  }
+
+  fs.readFile(__dirname + file, function (err, data) {
     if (err) {
       res.writeHead(500);
       return res.end('Error loading client.html');
@@ -19,4 +27,6 @@ function handler(req, res) {
 io.sockets.on('connection', function (socket) {
   socket.on('message', function () {});
   socket.on('disconnect', function () {});
+
+  console.log('Socket.io connected.');
 });
