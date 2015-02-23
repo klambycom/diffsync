7046184deea8ec632dc9e3c59acdb71c0b02f4d0
klambycom <christian@klamby.com>
2015-02-23 23:59:46 +0100

    Server

--- example/server.js
+++ example/server.js
@@ -1,4 +1,20 @@
-let io = require('socket.io').listen(80);
+let app = require('http').createServer(handler);
+let io = require('socket.io')(app);
+let fs = require('fs');
+
+app.listen(80);
+
+function handler(req, res) {
+  fs.readFile(__dirname + 'dist/example/client.html', function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading client.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
 io.sockets.on('connection', function (socket) {
   socket.on('message', function () {});
