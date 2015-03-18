e65ebd46dfca58e2420bc8ac0a33c7aae296b875
klambycom <christian@klamby.com>
2015-03-18 16:35:06 +0100

    Websocket in example

--- example/client.js
+++ example/client.js
@@ -1,37 +1,26 @@
-/*
-var { client } = require('../index');
 let socket = require('socket.io-client')('http://localhost:8000');
 
 socket.on('connect', function () {
-  var kollab = client(socket);
+  let diffsync = require('../index').client(socket);
+  let titleElem = document.querySelector('#app h1');
+  let docElem = document.querySelector('#app .document');
+  let sendElem = document.querySelector('#app .send');
 
-  kollab.update({
-    detta: 'ar',
-    bara: {
-      ett: 'test'
+  let doc = {
+    title: 'The title',
+    data: {
+      type: 'file',
+      changes: [
+      ]
     }
+  };
+
+  diffsync.update(doc);
+
+  titleElem.innerHTML = doc.title;
+  docElem.value = JSON.stringify(doc);
+
+  sendElem.addEventListener('click', e => {
+    console.log(JSON.parse(docElem.value));
   });
-
-  kollab.merge({ mer: 'ge' });
-});
-*/
-
-let doc = {
-  title: 'The title',
-  data: {
-    type: 'file',
-    changes: [
-    ]
-  }
-};
-
-let titleElem = document.querySelector('#app h1');
-let docElem = document.querySelector('#app .document');
-let sendElem = document.querySelector('#app .send');
-
-titleElem.innerHTML = doc.title;
-docElem.value = JSON.stringify(doc);
-
-sendElem.addEventListener('click', e => {
-  console.log(JSON.parse(docElem.value));
 });
