9fa905be6645e30b31c5c4b1761ca921c3e471b6
klambycom <christian@klamby.com>
2015-03-21 01:02:40 +0100

    More tests

--- test/client_test.js
+++ test/client_test.js
@@ -7,7 +7,15 @@ describe('Client', function () {
     sut = client({ on: function () {} });
   });
 
-  it('should have a function called update', function () {
+  it('should have a function called "update"', function () {
     expect(sut.update).toBeDefined();
+  });
+
+  it('should have a function called "merge"', function () {
+    expect(sut.merge).toBeDefined();
+  });
+
+  it('should have a function called "on"', function () {
+    expect(sut.on).toBeDefined();
   });
 });
