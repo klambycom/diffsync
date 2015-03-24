e5d73862aa2a5592b4517edf422c49ed3dfe8607
klambycom <christian@klamby.com>
2015-03-24 21:06:24 +0100

    Test if promise is returned

--- test/storage_driver_test.js
+++ test/storage_driver_test.js
@@ -16,12 +16,24 @@ describe('StorageDriver', function () {
       expect(sut._initStorage).toBeDefined();
     });
 
+    it('should return Promise when "_initStorage" is called', function () {
+      expect(sut._initStorage().constructor.name).toEqual('Promise');
+    });
+
     it('should have a function called "get"', function () {
       expect(sut.get).toBeDefined();
     });
 
+    it('should return Promise when "get" is called', function () {
+      expect(sut.get().constructor.name).toEqual('Promise');
+    });
+
     it('should have a function called "set"', function () {
       expect(sut.set).toBeDefined();
+    });
+
+    it('should return Promise when "set" is called', function () {
+      expect(sut.set().constructor.name).toEqual('Promise');
     });
   });
 });
