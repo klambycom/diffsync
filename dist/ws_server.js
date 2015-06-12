1b03d6c61ec1bb320eb17b1797f6e625820c65e4
klambycom <christian@klamby.com>
2015-06-13 00:52:36 +0200

    Connect-method

--- dist/ws_server.js
+++ dist/ws_server.js
@@ -4,28 +4,60 @@ var Promise = require('promise');
 var WebSocket = require('websocket').server;
 var http = require('http');
 
+var log = function log(msg) {
+  // TODO Only log i verbose
+  console.log('' + new Date() + ' ' + msg);
+};
+
 var server = http.createServer(function () {});
 var wsServer = null;
 var connected = false;
+
+var clients = [];
 
 var connect = function connect(port) {
   // Start server
   server.listen(port, function () {
     connected = true;
-    console.log(new Date() + ' Server is listening on port ' + port);
+    log('Server is listening on port ' + port + '.');
   });
   // Create websocket server
   wsServer = new WebSocket({ httpServer: server });
+  wsServer.on('request', function (request) {
+    log('Connection from origin ' + request.origin + '.');
+    // TOOD Check 'request.origin'
+    var connection = request.accept(null, request.origin);
+    var index = clients.push(connection) - 1;
+
+    log('Connection accepted.');
+
+    // User sent some message
+    connection.on('message', function (message) {
+      if (message.type === 'utf8') {
+        log('Received Message: "' + message.utf8Data + '".');
+        // TODO Use on('message', ...) below!
+      }
+    });
+
+    // User disconnected
+    connection.on('close', function (closedConnection) {
+      log('Peer ' + closedConnection.remoteAddress + ' disconnected.');
+      clients.splice(index, 1);
+      // TODO Use on('disconnected', ...) below!
+    });
+  });
 };
 
 // Send message to client
-var send = function send(msg) {};
+var broadcast = function broadcast(msg) {
+  var json = JSON.stringify({ type: 'message', data: msg });
+  clients.forEach(function (x) {
+    x.sendUTF(json);
+  });
+};
 
-// Receive mesage from server
-var receive = function receive() {};
-
-// Connect/disconnect/error
-var on = function on() {};
+// Connected/disconnected/error/message
+var on = function on(type, listener) {};
 
 module.exports = { send: send, receive: receive, on: on };
 /* empty */
--- src/ws_server.js
+++ src/ws_server.js
@@ -2,30 +2,58 @@ let Promise = require('promise');
 let WebSocket = require('websocket').server;
 let http = require('http');
 
+let log = function (msg) {
+  // TODO Only log i verbose
+  console.log(`${new Date()} ${msg}`);
+};
+
 let server = http.createServer(() => { /* empty */ });
 let wsServer = null;
 let connected = false;
+
+let clients = [];
 
 let connect = function (port) {
   // Start server
   server.listen(port, () => {
     connected = true;
-    console.log((new Date()) + " Server is listening on port " + port);
+    log(`Server is listening on port ${port}.`);
   });
   // Create websocket server
   wsServer = new WebSocket({ httpServer: server });
+  wsServer.on('request', request => {
+    log(`Connection from origin ${request.origin}.`);
+    // TOOD Check 'request.origin'
+    let connection = request.accept(null, request.origin);
+    let index = clients.push(connection) - 1;
+
+    log('Connection accepted.');
+
+    // User sent some message
+    connection.on('message', message => {
+      if (message.type === 'utf8') {
+        log(`Received Message: "${message.utf8Data}".`);
+        // TODO Use on('message', ...) below!
+      }
+    });
+
+    // User disconnected
+    connection.on('close', closedConnection => {
+      log(`Peer ${closedConnection.remoteAddress} disconnected.`);
+      clients.splice(index, 1);
+        // TODO Use on('disconnected', ...) below!
+    });
+  });
 };
 
 // Send message to client
-let send = function (msg) {
+let broadcast = function (msg) {
+  let json = JSON.stringify({ type: 'message', data: msg });
+  clients.forEach(x => { x.sendUTF(json); });
 };
 
-// Receive mesage from server
-let receive = function () {
-};
-
-// Connect/disconnect/error
-let on = function () {
+// Connected/disconnected/error/message
+let on = function (type, listener) {
 };
 
 module.exports = { send, receive, on };
