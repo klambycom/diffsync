1eff15fecaa1fecca1ba2d96224ae64967d3090a
klambycom <christian@klamby.com>
2015-06-14 00:54:10 +0200

    WS client stub

--- dist/ws_client.js
+++ dist/ws_client.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var connect = function connect() {};
+
+var send = function send() {};
+
+var addListener = function addListener() {};
+
+module.exports = { connect: connect, send: send, addListener: addListener };
---
+++ src/ws_client.js
@@ -0,0 +1,7 @@
+let connect = function () {};
+
+let send = function () {};
+
+let addListener = function () {};
+
+module.exports = { connect, send, addListener };
