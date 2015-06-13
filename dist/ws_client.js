6e7696f51f736f1ba3d700fa36b69d185be95bf9
klambycom <christian@klamby.com>
2015-06-14 01:06:07 +0200

    Add websocket

--- dist/ws_client.js
+++ dist/ws_client.js
@@ -1,9 +1,22 @@
-"use strict";
+/* global -WebSocket */
+'use strict';
 
-var connect = function connect() {};
+var WebSocket = require('websocket').server;
+//let WebSocket = window.WebSocket || window.MozWebSocket || require('websocket').server;
+
+var settings = {};
+settings.url = 'ws://127.0.0.1:8000';
+
+var connect = function connect() {
+  var connection = new WebSocket(settings.url);
+  connection.onopen = function () {};
+  connection.onerror = function (error) {};
+  connection.onmessage = function (msg) {};
+};
 
 var send = function send() {};
 
 var addListener = function addListener() {};
 
 module.exports = { connect: connect, send: send, addListener: addListener };
+/* TODO */ /* TODO */ /* TODO */
--- src/ws_client.js
+++ src/ws_client.js
@@ -1,4 +1,16 @@
-let connect = function () {};
+/* global -WebSocket */
+let WebSocket = require('websocket').server;
+//let WebSocket = window.WebSocket || window.MozWebSocket || require('websocket').server;
+
+let settings = {};
+settings.url = 'ws://127.0.0.1:8000';
+
+let connect = function () {
+  let connection = new WebSocket(settings.url);
+  connection.onopen = () => { /* TODO */ };
+  connection.onerror = error => { /* TODO */ };
+  connection.onmessage = msg => { /* TODO */ };
+};
 
 let send = function () {};
 
