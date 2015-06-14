387784552772a1a8cd8b589856b01e83786ee44a
klambycom <christian@klamby.com>
2015-06-14 14:46:24 +0200

    ws client implementation

--- dist/ws_client.js
+++ dist/ws_client.js
@@ -3,20 +3,34 @@
 
 var WebSocket = require('websocket').server;
 //let WebSocket = window.WebSocket || window.MozWebSocket || require('websocket').server;
+var EventEmitter = require('events').EventEmitter;
+
+var events = new EventEmitter();
 
 var settings = {};
 settings.url = 'ws://127.0.0.1:8000';
 
 var connect = function connect() {
   var connection = new WebSocket(settings.url);
-  connection.onopen = function () {};
-  connection.onerror = function (error) {};
-  connection.onmessage = function (msg) {};
+
+  connection.onerror = function (error) {
+    return events.emit('error', error);
+  };
+  connection.onmessage = function (msg) {
+    return events.emit('message', msg);
+  };
+
+  // Connected to WebSocket-server
+  connection.onopen = function () {
+    // TODO Wait for init document, and send it along with the event.
+    events.emit('connected', {});
+  };
 };
 
 var send = function send() {};
 
-var addListener = function addListener() {};
+var addListener = function addListener(event, listener) {
+  events.on(event, listener);
+};
 
 module.exports = { connect: connect, send: send, addListener: addListener };
-/* TODO */ /* TODO */ /* TODO */
--- src/ws_client.js
+++ src/ws_client.js
@@ -1,19 +1,47 @@
 /* global -WebSocket */
 let WebSocket = require('websocket').server;
 //let WebSocket = window.WebSocket || window.MozWebSocket || require('websocket').server;
+let EventEmitter = require('events').EventEmitter;
+
+let events = new EventEmitter();
+let connection;
+let connected = false;
 
 let settings = {};
 settings.url = 'ws://127.0.0.1:8000';
 
-let connect = function () {
-  let connection = new WebSocket(settings.url);
-  connection.onopen = () => { /* TODO */ };
-  connection.onerror = error => { /* TODO */ };
-  connection.onmessage = msg => { /* TODO */ };
+let _message = function (msg) {
+  try {
+    let json = JSON.parse(msg.data);
+    // TODO Maybe check type to deside the event
+    events.emit('message', msg);
+  } catch (e) {
+    console.log(`This doesn't look like a valid JSON: ${msg.data}`);
+  }
 };
 
-let send = function () {};
+let connect = function () {
+  connection = new WebSocket(settings.url);
 
-let addListener = function () {};
+  connection.onerror = error => events.emit('error', error);
+  connection.onmessage = _message;
+
+  // Connected to WebSocket-server
+  connection.onopen = () => {
+    connected = true;
+    // TODO Wait for init document, and send it along with the event.
+    events.emit('connected', {});
+  };
+};
+
+let send = function (msg) {
+  if (!connected) { return; }
+  // TODO Use JSON?
+  connection.send(msg);
+};
+
+let addListener = function (event, listener) {
+  events.on(event, listener);
+};
 
 module.exports = { connect, send, addListener };
