0b726c4a0000fd2cab4150efb1d9919c48f2054d
klambycom <christian@klamby.com>
2015-06-21 21:12:59 +0200

    Fix lint errors

--- dist/users.js
+++ dist/users.js
@@ -6,7 +6,7 @@ var clients = [];
 var _createUUID = function _createUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0,
-        v = c == 'x' ? r : r & 3 | 8;
+        v = c == 'x' ? r : r & 3 | 8; // jshint ignore:line
     return v.toString(16);
   });
 };
--- src/users.js
+++ src/users.js
@@ -3,7 +3,7 @@ let clients = [];
 // Source: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 let _createUUID = function () {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8); // jshint ignore:line
     return v.toString(16);
   });
 };
